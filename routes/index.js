3a93d77ddf688d6e426e399323a5f3056e3ac891
Matheus Galego <wespringx@gmail.com>
2024-03-27 21:44:41 -0300

    salvar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
-
+const router = express.Router();
+const mysql = require('mysql2/promise');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+ mysql.createConnection({host: 'localhost',user: 'Matheus',password: 'aula',
+ database: 'trabalhos',port: 3306
+ }).then((connection) => {connection.query('SELECT * FROM trabalhos;')
+ .then((result) => {res.send(result[0]);});
+ });
 });
-
 module.exports = router;
